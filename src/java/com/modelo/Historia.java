fff5acc03115a9b3b9cd93195af2f49edf6eaee6
Fabian Toapanta <fabianRBN_95@hotmail.com>
2016-08-20 12:49:01 -0500

    Configuracion de la conexion con historiasdb

---
+++ src/java/com/modelo/Historia.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Fabian
+ */
+@Entity
+@Table(name = "historia")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Historia.findAll", query = "SELECT h FROM Historia h"),
+    @NamedQuery(name = "Historia.findByIdHistoria", query = "SELECT h FROM Historia h WHERE h.idHistoria = :idHistoria"),
+    @NamedQuery(name = "Historia.findByNombreHistoria", query = "SELECT h FROM Historia h WHERE h.nombreHistoria = :nombreHistoria"),
+    @NamedQuery(name = "Historia.findByFechaHistoria", query = "SELECT h FROM Historia h WHERE h.fechaHistoria = :fechaHistoria")})
+public class Historia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_historia")
+    private Integer idHistoria;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "nombre_historia")
+    private String nombreHistoria;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "descripcion_historia")
+    private String descripcionHistoria;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_historia")
+    @Temporal(TemporalType.DATE)
+    private Date fechaHistoria;
+    @JoinColumn(name = "id_lugar", referencedColumnName = "id_lugar")
+    @ManyToOne(optional = false)
+    private Lugar idLugar;
+
+    public Historia() {
+    }
+
+    public Historia(Integer idHistoria) {
+        this.idHistoria = idHistoria;
+    }
+
+    public Historia(Integer idHistoria, String nombreHistoria, String descripcionHistoria, Date fechaHistoria) {
+        this.idHistoria = idHistoria;
+        this.nombreHistoria = nombreHistoria;
+        this.descripcionHistoria = descripcionHistoria;
+        this.fechaHistoria = fechaHistoria;
+    }
+
+    public Integer getIdHistoria() {
+        return idHistoria;
+    }
+
+    public void setIdHistoria(Integer idHistoria) {
+        this.idHistoria = idHistoria;
+    }
+
+    public String getNombreHistoria() {
+        return nombreHistoria;
+    }
+
+    public void setNombreHistoria(String nombreHistoria) {
+        this.nombreHistoria = nombreHistoria;
+    }
+
+    public String getDescripcionHistoria() {
+        return descripcionHistoria;
+    }
+
+    public void setDescripcionHistoria(String descripcionHistoria) {
+        this.descripcionHistoria = descripcionHistoria;
+    }
+
+    public Date getFechaHistoria() {
+        return fechaHistoria;
+    }
+
+    public void setFechaHistoria(Date fechaHistoria) {
+        this.fechaHistoria = fechaHistoria;
+    }
+
+    public Lugar getIdLugar() {
+        return idLugar;
+    }
+
+    public void setIdLugar(Lugar idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idHistoria != null ? idHistoria.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Historia)) {
+            return false;
+        }
+        Historia other = (Historia) object;
+        if ((this.idHistoria == null && other.idHistoria != null) || (this.idHistoria != null && !this.idHistoria.equals(other.idHistoria))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.modelo.Historia[ idHistoria=" + idHistoria + " ]";
+    }
+    
+}
---
+++ src/java/com/modelo/Lugar.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modelo;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Fabian
+ */
+@Entity
+@Table(name = "lugar")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Lugar.findAll", query = "SELECT l FROM Lugar l"),
+    @NamedQuery(name = "Lugar.findByIdLugar", query = "SELECT l FROM Lugar l WHERE l.idLugar = :idLugar"),
+    @NamedQuery(name = "Lugar.findByNombreLugar", query = "SELECT l FROM Lugar l WHERE l.nombreLugar = :nombreLugar"),
+    @NamedQuery(name = "Lugar.findByFechaLugar", query = "SELECT l FROM Lugar l WHERE l.fechaLugar = :fechaLugar")})
+public class Lugar implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_lugar")
+    private Integer idLugar;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "nombre_lugar")
+    private String nombreLugar;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "descripcion_lugar")
+    private String descripcionLugar;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_lugar")
+    @Temporal(TemporalType.DATE)
+    private Date fechaLugar;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idLugar")
+    private Collection<Historia> historiaCollection;
+
+    public Lugar() {
+    }
+
+    public Lugar(Integer idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    public Lugar(Integer idLugar, String nombreLugar, String descripcionLugar, Date fechaLugar) {
+        this.idLugar = idLugar;
+        this.nombreLugar = nombreLugar;
+        this.descripcionLugar = descripcionLugar;
+        this.fechaLugar = fechaLugar;
+    }
+
+    public Integer getIdLugar() {
+        return idLugar;
+    }
+
+    public void setIdLugar(Integer idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    public String getNombreLugar() {
+        return nombreLugar;
+    }
+
+    public void setNombreLugar(String nombreLugar) {
+        this.nombreLugar = nombreLugar;
+    }
+
+    public String getDescripcionLugar() {
+        return descripcionLugar;
+    }
+
+    public void setDescripcionLugar(String descripcionLugar) {
+        this.descripcionLugar = descripcionLugar;
+    }
+
+    public Date getFechaLugar() {
+        return fechaLugar;
+    }
+
+    public void setFechaLugar(Date fechaLugar) {
+        this.fechaLugar = fechaLugar;
+    }
+
+    @XmlTransient
+    public Collection<Historia> getHistoriaCollection() {
+        return historiaCollection;
+    }
+
+    public void setHistoriaCollection(Collection<Historia> historiaCollection) {
+        this.historiaCollection = historiaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idLugar != null ? idLugar.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Lugar)) {
+            return false;
+        }
+        Lugar other = (Lugar) object;
+        if ((this.idLugar == null && other.idLugar != null) || (this.idLugar != null && !this.idLugar.equals(other.idLugar))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.modelo.Lugar[ idLugar=" + idLugar + " ]";
+    }
+    
+}
